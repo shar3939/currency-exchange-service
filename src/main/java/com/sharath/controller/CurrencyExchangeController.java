b861d6e09a9991bcba31894b48c7fcdc585b528c
shar3939 <catchshar@gmail.com>
2019-06-19 22:18:17 +0530

    falut tolerance - circuit breaker example changes

--- src/main/java/com/sharath/controller/CurrencyExchangeController.java
+++ src/main/java/com/sharath/controller/CurrencyExchangeController.java
@@ -5,12 +5,15 @@ package com.sharath.controller;
 
 import java.math.BigDecimal;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.sharath.model.ExchangeValue;
 import com.sharath.repository.ExchangeRepository;
 
@@ -27,9 +30,28 @@ public class CurrencyExchangeController {
 	@Autowired
 	private ExchangeRepository repository;
 	
+	private Logger log = LoggerFactory.getLogger(this.getClass());
+	
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
-	public ExchangeValue retrieveUSDtoINR(@PathVariable String from, @PathVariable String to) {
+	public ExchangeValue retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
 		ExchangeValue exchngValue = repository.findByFromAndTo(from, to); 
+		exchngValue.setPort(Integer.parseInt(env.getProperty("local.server.port")));
+		return exchngValue;  
+	}
+	
+	@GetMapping("/currency-exchange-cb/from/{from}/to/{to}")
+	@HystrixCommand(fallbackMethod = "dummyRetrieveExchangeValue")
+	public ExchangeValue retrieveExchangeValueCB(@PathVariable String from, @PathVariable String to) {
+		/*
+		 * ExchangeValue exchngValue = repository.findByFromAndTo(from, to);
+		 * exchngValue.setPort(Integer.parseInt(env.getProperty("local.server.port")));
+		 */
+		throw new RuntimeException("RUN TIME EXCEPTION");  
+	}
+	
+	public ExchangeValue dummyRetrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
+		ExchangeValue exchngValue = new ExchangeValue(99999L, from, to, new BigDecimal(60)); 
+		log.error("From ERROR Fall back method");
 		exchngValue.setPort(Integer.parseInt(env.getProperty("local.server.port")));
 		return exchngValue;  
 	}
